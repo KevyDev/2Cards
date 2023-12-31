bf2a347704edbe8fe269c1019bf797dfe22ad3ad
KevyDev <kevydev@outlook.com>
2023-12-31 13:27:42 -0500

    Fixing cards bug and play again button hover

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "tailwindcss/tailwind.css"
 import {useEffect, useState} from "react"
 
+interface Pair {
+    image: string,
+    selected: boolean
+}
+
 const fetchCards = async (setCards: Function, setError: Function) => {
     fetch("https://rickandmortyapi.com/api/character")
     .then(data => data.json())
@@ -19,25 +24,25 @@ const selectPairs = (cards: string[]) => {
 
 export default function App() {
     let [loaded, setLoaded] = useState<boolean>(false),
-        [cards, setCards] = useState([]),
+        [allCards, setAllCards] = useState<string[]>([]),
         [error, setError] = useState<boolean>(false),
-        [currentPairs, setCurrentPairs] = useState<any[]>([]),
+        [currentPairs, setCurrentPairs] = useState<Pair[]>([]),
         [activeCards, setActiveCards] = useState<number[]>([]),
         [wrongPairs, setWrongPairs] = useState<number>(0),
         [ended, setEnded] = useState<boolean>(false)
 
     useEffect(() => {
-        fetchCards(setCards, setError)
+        fetchCards(setAllCards, setError)
     }, [])
 
-    const loadPairs = () => setCurrentPairs(selectPairs([...cards]))
+    const loadPairs = () => setCurrentPairs(selectPairs([...allCards]))
 
-    useEffect(function initGame () {
-        if(cards.length > 0) {
+    useEffect(() => {
+        if(allCards.length > 0) {
             loadPairs()
             setTimeout(() => setLoaded(true), 500)
         }
-    }, [cards])
+    }, [allCards])
 
     const selectCard = (index: number) => setActiveCards(prevActiveCards => ([...prevActiveCards, index]))
 
@@ -81,17 +86,17 @@ export default function App() {
             {ended && (
                 <div className={"fixed top-0 bottom-0 left-0 right-0 z-20 flex flex-col gap-4 items-center justify-center bg-white"}>
                     <h1 className="text-gray-500 text-2xl">You were wrong {wrongPairs} times!</h1>
-                    <button onClick={restartGame} className="bg-blue-500 px-8 py-2 text-xl text-white rounded hover:shadow">Play again</button>
+                    <button onClick={restartGame} className="bg-blue-500 hover:bg-blue-400 duration-200 px-8 py-2 text-xl text-white rounded hover:shadow">Play again</button>
                 </div>
             )}
             {currentPairs.length > 0 && (
                 <ul className="grid grid-cols-4 sm:grid-cols-5 p-4 gap-2">
-                    {currentPairs.map(({image, selected}, index:number) => {
-                        let active:Boolean = activeCards[0] === index || activeCards[1] === index
+                    {currentPairs.map(({image, selected}: Pair, index:number) => {
+                        let active:Boolean = activeCards.length > 0 && (activeCards[0] === index || activeCards[1] === index)
                         return (
                             <li
                                 key={"card-" + index}
-                                onClick={() => !active && !selected && selectCard(index)}
+                                onClick={() => !active && !selected && activeCards.length < 2 && selectCard(index)}
                                 className={"duration-200 flex items-center justify-center" + (!active && !selected ? " hover:shadow-md cursor-pointer" : "")}
                             >
                                 <img
